62149459515c3779e04a8182f9043c34555a26a1
Artiom Voitenkov <avoitenkov@objectstyle.com>
2016-05-04 17:45:56 +0300

    Use Jetty default servlet for serving web console #4

--- graphql-webconsole/src/main/java/org/objectstyle/graphql/webconsole/WebConsoleModule.java
+++ graphql-webconsole/src/main/java/org/objectstyle/graphql/webconsole/WebConsoleModule.java
@@ -1,18 +1,18 @@
 package org.objectstyle.graphql.webconsole;
 
 import com.google.inject.Binder;
+import com.nhl.bootique.BQCoreModule;
 import com.nhl.bootique.ConfigModule;
 import com.nhl.bootique.jetty.JettyModule;
-import com.nhl.bootique.jetty.MappedServlet;
-
-import java.util.Arrays;
-import java.util.HashSet;
 
 public class WebConsoleModule extends ConfigModule {
 
 	@Override
 	public void configure(Binder binder) {
-        MappedServlet mappedServlet = new MappedServlet(new WebConsoleServlet(), new HashSet<>(Arrays.asList("/graphiql/*")));
-		JettyModule.contributeMappedServlets(binder).addBinding().toInstance(mappedServlet);
+        JettyModule.contributeStaticServlet(binder, "default", "/graphiql/*");
+
+        BQCoreModule.contributeProperties(binder)
+                .addBinding("bq.jetty.staticResourceBase")
+                .toInstance(WebConsoleModule.class.getResource("").getPath());
 	}
 }
